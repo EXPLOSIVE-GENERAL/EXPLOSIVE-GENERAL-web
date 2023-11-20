5591bd3d45e0131740fcc91651ba4441650b3e8d
egoubeaux <goubeauxe@yahoo.com>
2023-11-20 15:37:54 -0500

    lab 5 final commit

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -5,19 +5,24 @@ import App from './App';
 describe('The main app', () => {
 
 	it('should render the Orders link', () => {
-	render(<App />); 
-	expect(screen.getByText('Orders')).toBeInTheDocument();
-});
+	  render(<App />); 
+	  expect(screen.getByText('Orders')).toBeInTheDocument();
+    });
+
+  it('should render the Payment link', () => {
+    render(<App />); 
+    expect(screen.getByText('Payment')).toBeInTheDocument();
+    });
  
   it('should render the Home link', () => {
-  render(<App />);
-  expect(screen.getByRole('link', { name: "Home" })).toBeInTheDocument();
-  });
+    render(<App />);
+    expect(screen.getByRole('link', { name: "Home" })).toBeInTheDocument();
+    });
 
   it('should render the Home header', () => {
     render(<App />);
     expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
-  });
+    });
   
 });
 
